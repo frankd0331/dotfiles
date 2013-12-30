72b3514401045fd731cb67454f7221b6e2c04824
Frank Dudek <dudek.f.p@gmail.com>
2013-12-29 20:50:24 -0600

    added the xorg.conf that supports qnix

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,15 @@
 import XMonad
 import XMonad.Actions.Volume
-import Data.Map    (fromList)
+import Data.Map.Lazy (fromList)
 import Data.Monoid (mappend)
 import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
 import XMonad.Layout.Fullscreen
+--import XMonad.Layout.NoBorders
 
 --this creates a three column tile
-myLayout = gaps [(U,18)] $ spacing 5 $ ThreeColMid 1 (3/100) (2/5) ||| ThreeCol 1 (3/100) (-7/24) ||| tiled ||| Mirror tiled ||| fullscreenFull Full 
+myLayout = gaps [(U,18)] $ spacing 5 $ ThreeColMid 1 (3/100) (2/5) ||| ThreeCol 1 (3/100) (-7/24) ||| tiled ||| Mirror tiled
   where tiled = Tall 1 (3/100) (1/2)
 
 main = do
