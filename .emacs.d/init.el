87aa671eb242b45218175e32df9a3411364b2685
Frank Dudek <dudek.f.p@gmail.com>
2014-06-08 11:23:17 -0500

    added custom c++ mode to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -173,5 +173,18 @@
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
-;; to-do refactor all of these separate packages into a my-packages list
-;; add:  cider
+;; c++
+(c-add-style "my-cpp-style"
+             '("stroustrup"
+               (indent-tabs-mode . nil)
+               (c-basic-offset . 4)
+               (c-offsets-alist . ((inline-open . 0)
+                                   (brace-list-open . 0)
+                                   (statement-case-open . +)))))
+
+(defun my-c++-mode-hook ()
+  (c-set-style "my-cpp-style")
+  (auto-fill-mode)
+  (c-toggle-auto-hungry-state 1))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
